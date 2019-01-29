3d475b20cc7d34732bf5e45d8557da41e9eb4043
Sean Chavez <realseanchavez@gmail.com>
2019-01-28 19:49:17 -0800

    connect to Rinkeby; check balance

--- c-web-3/index.js
+++ c-web-3/index.js
@@ -1,10 +1,10 @@
 const Web3 = require('web3');
 
-const url = 'https://mainnet.infura.io/v3/cf9e60827ece40c79e3c8206dcdf8f52';
+const url = 'https://rinkeby.infura.io/v3/cf9e60827ece40c79e3c8206dcdf8f52';
 
 const web3 = new Web3(url);
 
-const address = '0x53d284357ec70cE289D6D64134DfAc8E511c8a3D';
+const address = '0x104afa98fbd407c28e7336988d9fff9378c36b36';
 
 web3.eth.getBalance(address, (err, bal) => {
   const eth = web3.utils.fromWei(bal, 'ether');
