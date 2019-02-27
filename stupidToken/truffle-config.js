e11250047f63f5f50e1e642e2f341ddcce1306ce
Sean Chavez <realseanchavez@gmail.com>
2019-02-27 15:35:14 -0800

    deploy to rinkeby

--- stupidToken/truffle-config.js
+++ stupidToken/truffle-config.js
@@ -62,7 +62,7 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    ropsten: {
+    rinkeby: {
       provider: () =>
         new HDWalletProvider(
           mnemonic,
