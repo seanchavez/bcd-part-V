99a059363c83e7afee9235eb3dd9f3561fa65f40
Sean Chavez <realseanchavez@gmail.com>
2019-02-27 18:04:54 -0800

    implement StarNotary

---
+++ starNotaryv1/contracts/StarNotary.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.4.24;
+
+contract StarNotary {
+    string public starName;
+    address public starOwner;
+    
+    event starClaimed(address owner);
+    
+    constructor() public {
+        starName = "Shitty Udacity Star.";
+    }
+    
+    function claimStar() public {
+        starOwner = msg.sender;
+        emit starClaimed(msg.sender);
+    } 
+}
