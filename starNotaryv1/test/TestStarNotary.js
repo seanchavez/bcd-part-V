8b5be8c604df4c6254e5c4cbb6b5dc064c72924a
Sean Chavez <realseanchavez@gmail.com>
2019-02-28 09:48:23 -0800

    more tests

--- starNotaryv1/test/TestStarNotary.js
+++ starNotaryv1/test/TestStarNotary.js
@@ -5,14 +5,33 @@ const StarNotary = artifacts.require('StarNotary');
 
 let accounts;
 let owner;
+let secondUser;
 
 contract('StarNotary', accs => {
   accounts = accs;
   owner = accounts[0];
+  secondUser = accounts[1];
 });
 
 it('has correct name', async () => {
   const instance = await StarNotary.deployed();
   const starName = await instance.starName.call();
   assert.equal(starName, 'Shitty Udacity Star.');
+});
+
+it('can be claimed', async () => {
+  const instance = await StarNotary.deployed();
+  await instance.claimStar({ from: owner });
+  const starOwner = await instance.starOwner.call();
+  assert.equal(starOwner, owner);
+});
+
+it('can change owners', async () => {
+  const instance = await StarNotary.deployed();
+  await instance.claimStar({ from: owner });
+  let starOwner = await instance.starOwner.call();
+  assert.equal(starOwner, owner);
+  await instance.claimStar({ from: secondUser });
+  const secondOwner = await instance.starOwner.call();
+  assert.equal(secondOwner, secondUser);
 });
