d55c60d96a569029e63351dff5c17f32096b340f
Sean Chavez <realseanchavez@gmail.com>
2019-02-28 08:32:30 -0800

    add first test

--- starNotaryv1/test/TestStarNotary.js
+++ starNotaryv1/test/TestStarNotary.js
@@ -1,9 +1,18 @@
+// import { isMainThread } from 'worker_threads';
+// import { AssertionError } from 'assert';
+
 const StarNotary = artifacts.require('StarNotary');
 
-var accounts;
-var owner;
+let accounts;
+let owner;
 
 contract('StarNotary', accs => {
   accounts = accs;
   owner = accounts[0];
+});
+
+it('has correct name', async () => {
+  const instance = await StarNotary.deployed();
+  const starName = await instance.starName.call();
+  assert.equal(starName, 'Shitty Udacity Star.');
 });
