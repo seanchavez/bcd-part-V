7562941131cbcf23cbabd0aa58d8f81f40b57582
Sean Chavez <realseanchavez@gmail.com>
2019-03-01 09:48:26 -0800

    modify index.js

--- starNotaryV2/app/src/index.js
+++ starNotaryV2/app/src/index.js
@@ -1,5 +1,5 @@
-import Web3 from "web3";
-import metaCoinArtifact from "../../build/contracts/MetaCoin.json";
+import Web3 from 'web3';
+import starNotaryArtifact from '../../build/contracts/StarNotary.json';
 
 const App = {
   web3: null,
@@ -12,9 +12,9 @@ const App = {
     try {
       // get contract instance
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = metaCoinArtifact.networks[networkId];
+      const deployedNetwork = starNotaryArtifact.networks[networkId];
       this.meta = new web3.eth.Contract(
-        metaCoinArtifact.abi,
+        starNotaryArtifact.abi,
         deployedNetwork.address,
       );
 
@@ -24,51 +24,32 @@ const App = {
 
       this.refreshBalance();
     } catch (error) {
-      console.error("Could not connect to contract or chain.");
+      console.error('Could not connect to contract or chain.');
     }
   },
 
-  refreshBalance: async function() {
-    const { getBalance } = this.meta.methods;
-    const balance = await getBalance(this.account).call();
-
-    const balanceElement = document.getElementsByClassName("balance")[0];
-    balanceElement.innerHTML = balance;
-  },
-
-  sendCoin: async function() {
-    const amount = parseInt(document.getElementById("amount").value);
-    const receiver = document.getElementById("receiver").value;
-
-    this.setStatus("Initiating transaction... (please wait)");
-
-    const { sendCoin } = this.meta.methods;
-    await sendCoin(receiver, amount).send({ from: this.account });
-
-    this.setStatus("Transaction complete!");
-    this.refreshBalance();
-  },
-
   setStatus: function(message) {
-    const status = document.getElementById("status");
+    const status = document.getElementById('status');
     status.innerHTML = message;
   },
+
+  createStar: async function() {},
 };
 
 window.App = App;
 
-window.addEventListener("load", function() {
+window.addEventListener('load', function() {
   if (window.ethereum) {
     // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
     window.ethereum.enable(); // get permission to access accounts
   } else {
     console.warn(
-      "No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",
+      'No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live',
     );
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     App.web3 = new Web3(
-      new Web3.providers.HttpProvider("http://127.0.0.1:9545"),
+      new Web3.providers.HttpProvider('http://127.0.0.1:9545'),
     );
   }
 
