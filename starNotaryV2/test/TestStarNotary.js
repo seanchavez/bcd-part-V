7a42b6dd1771439a3fe91c5037df7d7482ef8c5e
Sean Chavez <realseanchavez@gmail.com>
2019-02-28 21:26:35 -0800

    add test, test the tests"

--- starNotaryV2/test/TestStarNotary.js
+++ starNotaryV2/test/TestStarNotary.js
@@ -24,3 +24,20 @@ it('lets user1 put their star up for sale', async () => {
   await instance.putStarUpForSale(starId, starPrice, { from: user1 });
   assert.equal(await instance.starsForSale.call(starId), starPrice);
 });
+
+it('lets user1 get the funds after the sale', async () => {
+  const instance = await StarNotary.deployed();
+  const user1 = accounts[1];
+  const user2 = accounts[2];
+  const starId = 3;
+  const starPrice = web3.utils.toWei('.01', 'ether');
+  const balance = web3.utils.toWei('.05', 'ether');
+  await instance.createStar('Shitty Star', starId, { from: user1 });
+  await instance.putStarUpForSale(starId, starPrice, { from: user1 });
+  const balanceOfUser1BeforeTransaction = await web3.eth.getBalance(user1);
+  await instance.buyStar(starId, { from: user2, value: balance });
+  const balanceOfUser1AfterTransaction = await web3.eth.getBalance(user1);
+  const value1 = Number(balanceOfUser1BeforeTransaction) + Number(starPrice);
+  const value2 = Number(balanceOfUser1AfterTransaction);
+  assert.equal(value1, value2);
+});
